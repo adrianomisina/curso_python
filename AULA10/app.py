e7d418bb6558ca096e5ffb8025f25ed9f431a9dc
adrianomisina <adriano.misina@gmail.com>
2024-10-01 04:36:10 -0300

    feat: colecoes set e dicionary

---
+++ AULA10/app.py
@@ -0,0 +1,23 @@
+# set usa colchetes
+# lista desordenada, não tem indice
+# não aceita valores duplicados
+
+# frutas = {"maça", "laranja", "abacaxi"}
+
+# para adicionar
+# frutas.add("Pera")
+
+#remover
+# frutas.remove("maça")
+
+#remove o ultimo elemento
+# frutas.pop()
+
+# set1 = {"maça", "laranja", "abacaxi"}
+# set2 = {0 , 3, 50, -74}
+# set3 = {True, False, False, True}
+# set4 = {"Roger", 34, True}
+
+# print(frutas)
+
+# print(set4)
