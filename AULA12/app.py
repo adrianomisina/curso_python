f34d8eaab54c76682ec29d0a0400ecf508d979a1
adrianomisina <adriano.misina@gmail.com>
2024-10-05 01:07:43 -0300

    feat: manipulacao de arquivos

--- AULA12/app.py
+++ AULA12/app.py
@@ -7,7 +7,8 @@
 # x - Criar Arquivo
 # r+ - Leitura  + Escrita
 
-arquivo = open("AULA12/text.txt", "r")
+# arquivo = open("AULA12/text.txt", "r")
+arquivo = open("AULA12/text.txt", "a")
 
 # print(arquivo.readable())
 # print(arquivo.read())
@@ -19,9 +20,24 @@ arquivo = open("AULA12/text.txt", "r")
 # print(arquivo.readline())
 # print(arquivo.readline())
 
-lista = arquivo.readlines()
+# lista = arquivo.readlines()
 # ['Pynthon\n', 'JavaScript\n', 'Java\n', 'C#\n', 'Node\n', 'R\n', 'React\n', 'Angular\n', 'Vue\n', 'Golang\n'] \n quebra de linha
-print(lista)
 
+# print(lista)
+# print(lista[3])
 
-arquivo.close()
+# arquivo.write("\nSQL")
+# arquivo.write("C++\n")
+
+# arquivo.close()
+
+# import os
+
+# if os.path.exit("AULA12/text.txt"):
+#   os.remove("AULA12/text.txt")
+# else: 
+#   print("Arquivo não existe")  
+
+# remover pasta apenas se tiver vazia:
+
+# os.rmdir("AULA12/nova-pasta")
